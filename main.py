7cd12371c1cf8b8c329dd8b9e62a213be1e21806
Denys12109 <denys.smetaniak@hebron-academy.com>
2023-10-09 15:08:54 +0300

    changes have been made_2

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ username = input()
 
 # пиши свій код починаючи з 5 строчки ->
 if 15 > len(username) > 3 and "@" not in username and "!" not in username: # перевіряю на довжину вводу символів та заборону знаків
- print("Username is valid")
+    print("Username is valid")
 else:  # виводжу повідомлення про правильність введення даних, якщо порушені умови
- print("Please, enter username from 3 to 15 characters and don't use ! or @")
+    print("Please, enter username from 3 to 15 characters and don't use ! or @")
