cb0657a5e9d64e9a6718963d4d9f25f9395b2833
anton19B <buksaanton5@gmail.com>
2023-10-10 12:13:40 +0300

    Solution

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ username = input ()
 
 # пиши свій код починаючи з 5 строчки ->
 if len(username) >= 3 and len(username) <= 15  and '!' not in username and '@' not in username:
-    print(Username is valid)
+    print("Username is valid")
 
 else :
     print ("Please, enter username from 3 to 15 characters and don't use ! or @")
