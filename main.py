d0ebdb7a19d216813b79819dedfbf47d801e34e8
anton19B <buksaanton5@gmail.com>
2023-10-10 10:04:20 +0300

    Selution

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 # не зміню наступну строку
-username = input()
+username = input ()
 
 # пиши свій код починаючи з 5 строчки ->
+def validate_username(username):
+    if 3 <= len(username) <= 15 and '!' not in username and '@' not in username:
+        return True
+    else:
+        return False
+
+# Приклад використання
+username = input("Введіть username: ")
+
+if validate_username(username):
+    print("Введений username є допустимим.")
+else:
+    print("Введений username не відповідає вимогам.")
