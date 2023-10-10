caacc2a00235d0a0b0c7761eaa32b3ede7fa34d5
2switch2 <graznoy842@gmail.com>
2023-10-10 16:24:26 +0300

    olution

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-# не зміню наступну строку
 username = input("Enter your username: ")
 
-# пиши свій код починаючи з 5 строчки ->
+# if 3 <= len(username) <= 15 and '!' or '@'not in username:
+#     print("Username is valid")
+# else:
+#     print("Please, enter a username from 3 to 15 characters and don't use '!' or '@'")
+
+if not(3 <= len(username) <= 15) or ('!' in username or '@' in username ):
+    print("Please, enter a username from 3 to 15 characters and don't use ! or @")
+    print("Please, enter username from 3 to 15 characters and don't use ! or @")
+else:
+    print("Username is valid")
