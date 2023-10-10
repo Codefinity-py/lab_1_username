4a6ef5f5cb04d9857a074975666a1cb2ce6a1263
Founddd <boneheadbulldog@gmail.com>
2023-10-10 12:58:47 +0300

    Solution

--- main.py
+++ main.py
@@ -2,3 +2,12 @@
 username = input("Enter your username: ")
 
 # пиши свій код починаючи з 5 строчки ->
+    #перевірка на кількість символів
+if len(username) >= 3 and len(username) <= 15:
+    # Перевірка на відсутність символів '!' та '@'
+    if '!' not in username and '@' not in username:
+        print("Username is valid")
+    else:
+        print("Please, enter username from 3 to 15 characters and don't use ! or @")
+else:
+    print("Please, enter username from 3 to 15 characters and don't use ! or @")
